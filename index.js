e8077bcdbeb878e928962b6c676f5e3b8fec71b6
Jake Rosoman <jkroso@gmail.com>
2016-06-27 01:24:19 +1200

    update tip content too

--- index.js
+++ index.js
@@ -81,7 +81,12 @@ export default class Tip extends Thunk {
   update(next, dom) {
     dom = super.update(next, dom)
     if (!(next instanceof Tip)) return dom
-    if (next.arguments[0].show === true) dom.tip.show()
+    const params = this.arguments[0]
+    if (dom.tip.el) {
+      dom.tip.node = next.arguments[0].content
+      params.content.update(dom.tip.node, dom.tip.el)
+    }
+    if (params.show === true) dom.tip.show()
     return dom
   }
 
